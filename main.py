45f0b30275ba16d0031370d1aeb311046e91b8c9
nikki-cmd <76265039+nikki-cmd@users.noreply.github.com>
2025-01-14 20:25:39 +0300

    Add data splited by classes, deleted stop-words, made vectorization on words

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from  preprocessing import get_data
 import logistic_regression as LogisticRegression
 
+print(get_data()[0])
--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,15 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 def divide_chunks(l, n):
     
     # looping till length l
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
-def get_data():
-    path = "datasets/Reviews.csv" 
-
-    df = pd.read_csv(path)
-    df = pd.DataFrame(df)
-
-    print(df.head(10))
-    print(df.columns)
-
-    vectorizer = TfidfVectorizer()
-    messages = list(df.Text)
-
-    x = list(divide_chunks(messages, 1000))
-            
+def get_key_words(x):
+    vectorizer = TfidfVectorizer(stop_words='english')
     keys = []
     for i in range(0, len(x)):
         matrix = vectorizer.fit_transform(x[i])
@@ -32,3 +21,25 @@ def get_data():
             keys.append(sorted_keywords[j])
             
     return keys
+
+def get_data():
+    path = "datasets/Reviews.csv" 
+
+    df = pd.read_csv(path)
+    df = pd.DataFrame(df)
+    
+    bad_df = df[(df.Score == 1) | (df.Score == 2)]
+    good_df = df[(df.Score == 3) | (df.Score == 4)]
+    excellent_df = df[df.Score == 5]
+    
+    x_bad = list(divide_chunks(list(bad_df.Text), 1000))
+    x_good = list(divide_chunks(list(good_df.Text), 1000))
+    x_excellent = list(divide_chunks(list(excellent_df.Text), 1000))
+            
+    keys_bad = get_key_words(x_bad)
+    keys_good = get_key_words(x_good)
+    keys_excellent = get_key_words(x_excellent)
+    
+    return [keys_bad, keys_good, keys_excellent]
+
+
