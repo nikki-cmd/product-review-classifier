084ddbeed3b21b2fe0634a2b1a348919434d4597
nikki-cmd <76265039+nikki-cmd@users.noreply.github.com>
2025-01-14 19:11:13 +0300

    Add imports

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import kagglehub
+from  preprocessing import get_data
+import logistic_regression as LogisticRegression
 
-# Download latest version
-path = kagglehub.dataset_download(
-    "arhamrumi/amazon-product-reviews")
-
-print("Path to dataset files:", path)
