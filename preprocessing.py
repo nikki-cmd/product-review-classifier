8d8f195ecb1363c5e075856a78df399f9e00533e
nikki-cmd <76265039+nikki-cmd@users.noreply.github.com>
2025-01-14 18:57:39 +0300

    Fix prep

--- preprocessing.py
+++ preprocessing.py
@@ -7,27 +7,28 @@ def divide_chunks(l, n):
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
-path = "datasets/Reviews.csv" 
+def get_data():
+    path = "datasets/Reviews.csv" 
 
-df = pd.read_csv(path)
-df = pd.DataFrame(df)
+    df = pd.read_csv(path)
+    df = pd.DataFrame(df)
 
-print(df.head(10))
-print(df.columns)
+    print(df.head(10))
+    print(df.columns)
 
-vectorizer = TfidfVectorizer()
-messages = list(df.Text)
+    vectorizer = TfidfVectorizer()
+    messages = list(df.Text)
 
-x = list(divide_chunks(messages, 1000))
-        
-keys = []
-for i in range(0, len(x)):
-    matrix = vectorizer.fit_transform(x[i])
+    x = list(divide_chunks(messages, 1000))
+            
+    keys = []
+    for i in range(0, len(x)):
+        matrix = vectorizer.fit_transform(x[i])
 
-    feature_names = vectorizer.get_feature_names_out()
-    scores = matrix.toarray()[0]
-    sorted_keywords = [word for _, word in sorted(zip(scores, feature_names), reverse=True)]
-    for j in range(0, len(sorted_keywords)):
-        keys.append(sorted_keywords[j])
-
-print(f"Keywords{keys}")
+        feature_names = vectorizer.get_feature_names_out()
+        scores = matrix.toarray()[0]
+        sorted_keywords = [word for _, word in sorted(zip(scores, feature_names), reverse=True)]
+        for j in range(0, len(sorted_keywords)):
+            keys.append(sorted_keywords[j])
+            
+    return keys
