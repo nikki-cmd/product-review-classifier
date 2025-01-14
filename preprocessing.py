dab13a0b2c741e197094febe53f0c955a625143d
nikki-cmd <76265039+nikki-cmd@users.noreply.github.com>
2025-01-14 18:52:34 +0300

    Fix preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+def divide_chunks(l, n):
+    
+    # looping till length l
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
+
 path = "datasets/Reviews.csv" 
 
 df = pd.read_csv(path)
@@ -9,18 +15,19 @@ df = pd.DataFrame(df)
 print(df.head(10))
 print(df.columns)
 
-comments = []
-
-for i in range(0, len(df.Text)):
-    comments.append(df.Text[i])
-
 vectorizer = TfidfVectorizer()
+messages = list(df.Text)
 
-matrix = vectorizer.fit_transform(comments[:1000])
+x = list(divide_chunks(messages, 1000))
+        
+keys = []
+for i in range(0, len(x)):
+    matrix = vectorizer.fit_transform(x[i])
 
-feature_names = vectorizer.get_feature_names_out()
-scores = matrix.toarray()[0]
+    feature_names = vectorizer.get_feature_names_out()
+    scores = matrix.toarray()[0]
+    sorted_keywords = [word for _, word in sorted(zip(scores, feature_names), reverse=True)]
+    for j in range(0, len(sorted_keywords)):
+        keys.append(sorted_keywords[j])
 
-sorted_keywords = [word for _, word in sorted(zip(scores, feature_names), reverse=True)]
-
-print(f"Keywords{sorted_keywords}")
+print(f"Keywords{keys}")
